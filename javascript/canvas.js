4fcac70e632c69a15584ed680b7bab8f65b9479c
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-24 16:10:09 +1000

    updating

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -9,6 +9,7 @@
 down = {};
 up = {};
 annotation_number = 0;
+annotate_mode = false;
   
 function getContentCallback(response) {
     if (response != null)
@@ -25,41 +26,50 @@ function getContentCallback(response) {
 
 function enableAnnotations() {
     
+    $("#make_annotation").click(function () {
+       annotate_mode = true;
+    });
+    
     $("#articleHTML").mousedown(function (evt) {
-       down.x = evt.pageX; 
-       down.y = evt.pageY; 
-       evt.preventDefault();
+        if (annotate_mode) {
+           down.x = evt.pageX; 
+           down.y = evt.pageY; 
+           evt.preventDefault();
+       }
        
     });
     
     $("#articleHTML").mouseup(function (evt) {
-        
-        up.x = evt.pageX;
-        up.y = evt.pageY;
-        
-        first = {};
-        second = {};
-        
-        first.x = Math.min(down.x, up.x);
-        first.y = Math.min(down.y, up.y);
-        
-        second.x = Math.max(down.x, up.x);
-        second.y = Math.max(down.y, up.y);
-        
-        width = second.x - first.x;
-        height = second.y - first.y;
-                
-        $("#articleHTML").append("<div class='annotation' style='border: 1px solid #f00; position: absolute; left:" + first.x + "px; top: " + first.y + "px; width: " + width + "px; height: " + height + "px; ' id='" + annotation_number + "'>annotation!!!!!!!!</div>");
-        $("#" + annotation_number).append("<div class='text_button' id='text_button_'" + annotation_number + " note_id='" + annotation_number + " ' >Click here!</div>");
-        $("#" + annotation_number).append("<div style='display:none' class='text_button' id='text_field_'" + annotation_number + " note_id='" + annotation_number + " ' >ewfffffffffffffffwefwef</div>");
-        
-        $("#text_button_" + annotation_number).click(function () {
-            $("#text_field_" + annotation_number).slideDown();
-        });
-        
-        
-        annotation_number += 1;
-        evt.preventDefault();
+        if (annotate_mode) {
+            up.x = evt.pageX;
+            up.y = evt.pageY;
+            
+            first = {};
+            second = {};
+            
+            first.x = Math.min(down.x, up.x);
+            first.y = Math.min(down.y, up.y);
+            
+            second.x = Math.max(down.x, up.x);
+            second.y = Math.max(down.y, up.y);
+            
+            width = second.x - first.x;
+            height = second.y - first.y;
+                    
+            $("#articleHTML").append("<div class='annotation' style='border: 1px solid #f00; position: absolute; left:" + first.x + "px; top: " + first.y + "px; width: " + width + "px; height: " + height + "px; ' id='" + annotation_number + "'>annotation!!!!!!!!</div>");
+            $("#" + annotation_number).append("<div class='text_button' id='text_button_'" + annotation_number + " note_id='" + annotation_number + " ' >Click here!</div>");
+            $("#" + annotation_number).append("<div style='display:none' class='text_button' id='text_field_'" + annotation_number + " note_id='" + annotation_number + " ' >ewfffffffffffffffwefwef</div>");
+            
+            $("#text_button_" + annotation_number).click(function () {
+                $("#text_field_" + annotation_number).slideDown();
+            });
+            
+            
+            annotation_number += 1;
+            evt.preventDefault();
+            
+            annotate_mode = false;
+        }
     });
 }
 
