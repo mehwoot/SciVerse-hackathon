273bfbb8b1df04d0959766826fa1088f1dcf6cb6
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-25 03:24:41 +1000

    updated

--- javascript/profile.js
+++ javascript/profile.js
@@ -6,9 +6,7 @@ function contextCallback(obj) {
     context_obj = obj;
     
     
-        $("#content").html("<iframe id='content_iframe' src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "/gadget' width='300' height='150'></iframe>");
-
-    $("#content").html("<a href='javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
+    $("#content").html("<iframe id='content_iframe' src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "/gadget' width='300' height='150'></iframe><a href='javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
     
     $("#annotate").click(function () {
         var canvas_view = new gadgets.views.View("canvas"); 
