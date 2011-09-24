4285b35f5c63ba46f47e0d302f21a76fed6b5afe
Will <me@willcannings.com>
2011-09-25 02:55:07 +1000

    test

--- javascript/profile.js
+++ javascript/profile.js
@@ -3,7 +3,7 @@ function getContextInfo() {
 }
 		
 function contextCallback(obj) {
-  $("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "?title=" + encodeURIComponent(obj.docTitle) + "' width='327' height='150' style='border: none; margin: 0px; padding: 0px'></iframe>");
+  $("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "/gadget?title=" + encodeURIComponent(obj.docTitle) + "' width='327' height='150' style='border: none; margin: 0px; padding: 0px'></iframe>");
   $("#content").append("<a href='javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
   $("#annotate").click(function () {
     var canvas_view = new gadgets.views.View("canvas"); 
