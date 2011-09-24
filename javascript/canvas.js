0d04e939ce309ccebdbc577c8e18a8cc3eef596e
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-24 13:54:15 +1000

    * Added javascript file to be included by the canvas part of the application

---
+++ javascript/canvas.js
@@ -0,0 +1,20 @@
+ /*
+  * Call back function to process content
+  *
+  * response - response from getArticleContent call, string of article page body
+  *
+  */
+ function getContentCallback(response)
+ {
+   if (response != null)
+   {
+     document.getElementById("articleHTML").innerHTML=response;
+   }
+          // Tells gadget to resize itself
+          gadgets.window.adjustHeight();
+ }
+
+
+    $(document).ready(function () {
+        gadgets.sciverse.getArticleContent(getContentCallback);
+    });
