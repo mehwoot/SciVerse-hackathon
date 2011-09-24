838d2b89b5b53345be9a2c508d312936c5feec86
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-24 16:16:09 +1000

    updating

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -58,11 +58,11 @@ function enableAnnotations() {
                     
             $("#articleHTML").append("<div class='annotation' style='border: 1px solid #f00; position: absolute; left:" + first.x + "px; top: " + first.y + "px; width: " + width + "px; height: " + height + "px; ' id='" + annotation_number + "'>annotation!!!!!!!!</div>");
             $("#" + annotation_number).append("<div class='text_button' id='text_button_" + annotation_number + "' note_id='" + annotation_number + " ' >Click here!</div>");
-            $("#" + annotation_number).append("<div style='display:none' class='text_button' id='text_field_" + annotation_number + "' note_id='" + annotation_number + " ' >ewfffffffffffffffwefwef</div>");
+            $("#" + annotation_number).append("<div class='text_button' id='text_field_" + annotation_number + "' note_id='" + annotation_number + " ' >ewfffffffffffffffwefwef</div>");
             
             $("#text_button_" + annotation_number).click(function () {
                 alert('lol');
-                $("#text_field_" + annotation_number).show();
+                $("#text_field_" + annotation_number).toggle();
             });
             
             
