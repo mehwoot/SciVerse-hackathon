b3b1a643245cb286ff6070ff683152b71254b997
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-24 17:25:59 +1000

    updating

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -59,10 +59,14 @@ function enableAnnotations() {
             $("#articleHTML").append("<div class='annotation' style='border: 1px solid #f00; position: absolute; left:" + first.x + "px; top: " + first.y + "px; width: " + width + "px; height: " + height + "px; ' id='annotation_" + annotation_number + "'></div>");
             
             $("#annotation_" + annotation_number).append("<div class='text_button' style='position:relative; text-align: center; top: " + height + "px; width: " + width + "px;' id='text_button_" + annotation_number + "' note_id='" + annotation_number + "' >Add text</div>");
-            $("#annotation_" + annotation_number).append("<div class='text_button' style='display:none; position:relative; top: " + height + "px; width: " + width + "px;' id='text_field_" + annotation_number + "' note_id='" + annotation_number + "' ><textarea rows='1' cols='15'></textarea><br/><input type='submit' value='submit' /></div>");
+            $("#annotation_" + annotation_number).append("<div class='text_button' style='display:none; position:relative; top: " + height + "px; width: " + width + "px;' id='text_field_" + annotation_number + "' note_id='" + annotation_number + "' ><textarea rows='1' cols='15'></textarea><br/><input type='submit' id='annotation_submit_" + annotation_number + "' value='submit' /></div>");
             
             $("#text_button_" + annotation_number).click(function () {
                 $("#text_field_" + $(this).attr("note_id")).slideToggle();
+            });
+            
+            $("#annotation_submit_" + annotation_number).click(function () {
+               $(this).parent().slideToggle(); 
             });
             
             
