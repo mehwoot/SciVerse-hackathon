df057d05d5a66d55891987894863fec9d5233a58
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-25 00:20:50 +1000

    updated

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -4,16 +4,15 @@
   * response - response from getArticleContent call, string of article page body
   *
   */
-  
-  
-function getContextInfo() {
-  gadgets.sciverse.getContextInfo(contextCallback);
-}
 		
-function contextCallback(obj) {
-    $("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "?title=" + encodeURIComponent(obj.docTitle) + "' width='1300' height='1500' style='border: none; margin: 0px; padding: 0px'></iframe>");
+function getContentCallback(obj) {
+    $("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "?title=" + encodeURIComponent(obj.docTitle) + "' width='1300' height='1600' style='border: none; margin: 0px; padding: 0px'></iframe>");
 }
 
 $(document).ready(function () {
-   getContextInfo(); 
+    
+    context_data = gadgets.views.getParams();
+    /* Get the article content */
+    gadgets.sciverse.getArticleContent(getContentCallback);
+    
 });
