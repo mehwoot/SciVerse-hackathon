b2c99fe11e35c13dcce4a0da3fa1ab419bb8f0a4
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-24 17:02:30 +1000

    updating

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -58,8 +58,8 @@ function enableAnnotations() {
                     
             $("#articleHTML").append("<div class='annotation' style='border: 1px solid #f00; position: absolute; left:" + first.x + "px; top: " + first.y + "px; width: " + width + "px; height: " + height + "px; ' id='annotation_" + annotation_number + "'></div>");
             
-            $("#annotation_" + annotation_number).append("<div class='text_button' style='position:relative; top: " + height + "px; width: " + (width /2) + "px;' id='text_button_" + annotation_number + "' note_id='" + annotation_number + "' >Click here!</div>");
-            $("#annotation_" + annotation_number).append("<div class='text_button' style='display:none' id='text_field_" + annotation_number + "' note_id='" + annotation_number + "' >ewfffffffffffffffwefwef</div>");
+            $("#annotation_" + annotation_number).append("<div class='text_button' style='position:relative; top: " + height + "px; left: " + (width /2) + "px;' id='text_button_" + annotation_number + "' note_id='" + annotation_number + "' >Click here!</div>");
+            $("#annotation_" + annotation_number).append("<input type='text' class='text_button' style='display:none position:relative; top: " + (height + 50) + "px; left: " + (width /2) + "px;' id='text_field_" + annotation_number + "' note_id='" + annotation_number + "' >ewfffffffffffffffwefwef</div>");
             
             $("#text_button_" + annotation_number).click(function () {
                 $("#text_field_" + $(this).attr("note_id")).slideToggle();
