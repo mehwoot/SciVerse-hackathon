b37ddd5c0b7cc71c8946f73feed90ab995ce1636
Will <me@willcannings.com>
2011-09-24 21:49:59 +1000

    Fb login test

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-
+  
+  FB.login(function(response) {
+     if (response.authResponse) {
+       console.log('Welcome!  Fetching your information.... ');
+       FB.api('/me', function(response) {
+         console.log('Good to see you, ' + response.name + '.');
+         FB.logout(function(response) {
+           console.log('Logged out.');
+         });
+       });
+     } else {
+       console.log('User cancelled login or did not fully authorize.');
+     }
+   }, {scope: ''});
+    
     /* Display the welcome text */
     $("#content").html("<a href='javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
     
