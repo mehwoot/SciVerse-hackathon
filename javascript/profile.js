a6d785ac6e2e38bdd63726f75306c0789cad897c
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-24 14:02:34 +1000

    updating

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,1 +1,5 @@
-$j("#content").html="Hey, open this in full screen view to markup this application!";
+$(document).ready(function() {
+
+    $("#content").html="Hey, open this in full screen view to markup this application!";
+    
+});
