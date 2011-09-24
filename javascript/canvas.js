ac667a3f223f00883c00061c0b366e97c3a5c479
Will <me@willcannings.com>
2011-09-25 02:54:04 +1000

    test

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -17,7 +17,7 @@ function getPageHtmlCallback(response) {
 }
 
 function getContentCallback(obj) {
-    $("#content").html("<iframe id='content_iframe' src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "?title=" + encodeURIComponent(obj.docTitle) + "' width='1300' height='700' style='border: none; margin: 0px; padding: 0px'></iframe>");
+    $("#content").html("<iframe id='content_iframe' src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "?title=" + encodeURIComponent(obj.docTitle) + "' width='1300' height='700' style='border: none; margin: 0px; padding: 0px; width: 100%; height: 100%'></iframe>");
     gadgets.sciverse.getArticleContent(getPageHtmlCallback);
 }
 
