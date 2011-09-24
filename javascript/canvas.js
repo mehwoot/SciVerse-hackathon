2e36f477cdc014d9c31dca5385d10cfc2c69680d
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-25 00:05:37 +1000

    updated

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -14,3 +14,7 @@ function contextCallback(obj) {
   $("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "?title=" + encodeURIComponent(obj.docTitle) + "' width='327' height='150' style='border: none; margin: 0px; padding: 0px'></iframe>");
   gadgets.window.adjustHeight();
 }
+
+$(document).ready(function () {
+   getContextInfo(); 
+});
