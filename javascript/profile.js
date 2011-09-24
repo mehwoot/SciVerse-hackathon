bcf872191e3d5ed9768f7722a65c6329d8d9da00
Will <me@willcannings.com>
2011-09-24 22:10:18 +1000

    Potentially finalised iframe callback

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,46 +1,10 @@
-function getContextInfo(){
-			gadgets.sciverse.getContextInfo(contextCallback);
-		}
+function getContextInfo() {
+  gadgets.sciverse.getContextInfo(contextCallback);
+}
 		
-		function contextCallback(obj){
+function contextCallback(obj) {
+  $("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "?title=" + encodeURIComponent(obj.docTitle) + "' width='327' height='150' style=""></iframe>");
+  gadgets.window.adjustHeight();
+}
 
-		    var rs = JSON.stringify(obj);
-		 
-		   //Format the string for display.
-		    rs = rs.replace(/",/g,"\",<br>");
-
-		    var contextInfo = document.getElementById("content");
-		    contextInfo.innerHTML = rs;
-		    
- 		    gadgets.window.adjustHeight();
- 		    
-		}
-
-		gadgets.util.registerOnLoadHandler(getContextInfo);
-		
-$(document).ready(function() {
-  
-  // FB.login(function(response) {
-  //    if (response.authResponse) {
-  //      console.log('Welcome!  Fetching your information.... ');
-  //      FB.api('/me', function(response) {
-  //        console.log('Good to see you, ' + response.name + '.');
-  //        FB.logout(function(response) {
-  //          console.log('Logged out.');
-  //        });
-  //      });
-  //    } else {
-  //      console.log('User cancelled login or did not fully authorize.');
-  //    }
-  //  }, {scope: ''});
-    
-    /* Display the welcome text */
-    //$("#content").html("<a href='javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
-    
-    //$("#annotate").click(function () {
-    //    var canvas_view = new gadgets.views.View("canvas"); 
-    //    gadgets.views.requestNavigateTo(canvas_view);
-    //});
-    
-    //$("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/1?title=hello+world' width='327' height='150' style=""></iframe>")
-});
+gadgets.util.registerOnLoadHandler(getContextInfo);
