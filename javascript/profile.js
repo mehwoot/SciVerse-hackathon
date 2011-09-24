4ddd9fe8a9365113977e125407b18438272ad818
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-24 14:28:40 +1000

    updating

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     /* Display the welcome text */
-    $("#content").html("<a 'javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
+    $("#content").html("<a href='javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
     
     $("#annotate").click(function () {
         var canvas_view = new gadgets.views.View("canvas"); 
