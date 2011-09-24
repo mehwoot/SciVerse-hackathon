1e1209f09bd6e2617b934ab9190d65711c8e9648
Will <me@willcannings.com>
2011-09-24 21:56:14 +1000

    testing an iframe

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
   
-  FB.login(function(response) {
-     if (response.authResponse) {
-       console.log('Welcome!  Fetching your information.... ');
-       FB.api('/me', function(response) {
-         console.log('Good to see you, ' + response.name + '.');
-         FB.logout(function(response) {
-           console.log('Logged out.');
-         });
-       });
-     } else {
-       console.log('User cancelled login or did not fully authorize.');
-     }
-   }, {scope: ''});
+  // FB.login(function(response) {
+  //    if (response.authResponse) {
+  //      console.log('Welcome!  Fetching your information.... ');
+  //      FB.api('/me', function(response) {
+  //        console.log('Good to see you, ' + response.name + '.');
+  //        FB.logout(function(response) {
+  //          console.log('Logged out.');
+  //        });
+  //      });
+  //    } else {
+  //      console.log('User cancelled login or did not fully authorize.');
+  //    }
+  //  }, {scope: ''});
     
     /* Display the welcome text */
-    $("#content").html("<a href='javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
+    //$("#content").html("<a href='javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
     
-    $("#annotate").click(function () {
-        var canvas_view = new gadgets.views.View("canvas"); 
-        gadgets.views.requestNavigateTo(canvas_view);
-    });
+    //$("#annotate").click(function () {
+    //    var canvas_view = new gadgets.views.View("canvas"); 
+    //    gadgets.views.requestNavigateTo(canvas_view);
+    //});
     
-    
-    
+    $("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/1?title=hello+world' width='327' height='150'></iframe>")
 });
