9e1192ce4ebcd1b11961571bf1c1e937e5276632
Gareth Robinson <gareth.robinson@smartservicescrc.com.au>
2011-09-25 00:04:48 +1000

    updated

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -6,99 +6,11 @@
   */
   
   
-down = {};
-up = {};
-annotation_number = 0;
-annotate_mode = false;
-context_data = {};
-annotations = {};
-annotation_selected = -1;
-
-function getContentCallback(response) {
-    if (response != null)
-    {
-        document.getElementById("articleHTML").innerHTML=response;
-    }
-    
-    enableAnnotations();
-    
+function getContextInfo() {
+  gadgets.sciverse.getContextInfo(contextCallback);
 }
-
-function updateDisplayComments() {
-    $("#comments").html("");
-    for (i=0; i<annotations[annotation_selected].comments.length; i++) {
-        $("#comments").append("<span class='comment'>" + annotations[annotation_selected].comments[i] + "</span><br/>");
-    }    
+		
+function contextCallback(obj) {
+  $("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "?title=" + encodeURIComponent(obj.docTitle) + "' width='327' height='150' style='border: none; margin: 0px; padding: 0px'></iframe>");
+  gadgets.window.adjustHeight();
 }
-
-function enableAnnotations() {
-    
-    $("#make_annotation").click(function () {
-       annotate_mode = true;
-    });
-    
-    $("#articleHTML").mousedown(function (evt) {
-        if (annotate_mode) {
-           down.x = evt.pageX; 
-           down.y = evt.pageY; 
-           evt.preventDefault();
-       }
-       
-    });
-    
-    $("#comment_submit").click(function () {
-        if (annotation_selected != -1) {
-            annotations[annotation_selected].comments.push($("#comment_input").attr("value"));
-            $("#comment_input").attr("value", "");
-            updateDisplayComments();
-        }
-    });
-    
-    $("#articleHTML").mouseup(function (evt) {
-        if (annotate_mode) {
-            up.x = evt.pageX;
-            up.y = evt.pageY;
-            
-            first = {};
-            second = {};
-            
-            first.x = Math.min(down.x, up.x);
-            first.y = Math.min(down.y, up.y);
-            
-            second.x = Math.max(down.x, up.x);
-            second.y = Math.max(down.y, up.y);
-            
-            width = second.x - first.x;
-            height = second.y - first.y;
-                    
-            $("#articleHTML").append("<div class='annotation selected' style='border: 1px solid #f00; position: absolute; left:" + first.x + "px; top: " + first.y + "px; width: " + width + "px; height: " + height + "px; ' id='annotation_" + annotation_number + "' ann_num='"+annotation_number+"'></div>");
-            
-            annotations[annotation_number] = { "x" : first.x, "y": first.y, "width": width, "height": height, comments : new Array(), "id" : annotation_number };
-            
-            
-            $("#annotation_" + annotation_number).click(function() {
-                ann_num = $(this).attr("ann_num");
-                annotation_selected = ann_num;
-                updateDisplayComments();
-                $(".annotation").removeClass("selected");
-                $(this).addClass("selected");
-            });
-            
-            annotation_selected = annotation_number;
-            updateDisplayComments();
-            
-            annotation_number += 1;
-            evt.preventDefault();
-            
-            annotate_mode = false;
-        }
-    });
-}
-
-$(document).ready(function () {
-    
-    context_data = gadgets.views.getParams();
-    /* Get the article content */
-    gadgets.sciverse.getArticleContent(getContentCallback);
-    
-});
--- javascript/profile.js
+++ javascript/profile.js
@@ -1,15 +1,4 @@
-function getContextInfo() {
-  gadgets.sciverse.getContextInfo(contextCallback);
-}
-		
-function contextCallback(obj) {
-  $("#content").html("<iframe src='http://sciverse-annotator.herokuapp.com/papers/" + obj.pii + "?title=" + encodeURIComponent(obj.docTitle) + "' width='327' height='150' style='border: none; margin: 0px; padding: 0px'></iframe>");
-  gadgets.window.adjustHeight();
-}
 
-gadgets.util.registerOnLoadHandler(getContextInfo);
-
-/*
 context_obj = {};
 
 function contextCallback(obj) {
@@ -33,4 +22,4 @@ $(document).ready(function() {
     
     
 });
-*/
+
