0d93c42f2eaf82efe9247ed89f9b9eb945755e17
Will <me@willcannings.com>
2011-09-25 02:30:25 +1000

    test

--- javascript/profile.js
+++ javascript/profile.js
@@ -7,7 +7,7 @@ function contextCallback(obj) {
   $("#content").append("<a href='javascript:void(0);' id='annotate'>Click here to annotate this article!</a>");
   $("#annotate").click(function () {
     var canvas_view = new gadgets.views.View("canvas"); 
-    gadgets.views.requestNavigateTo(canvas_view, context_obj);
+    gadgets.views.requestNavigateTo(canvas_view, obj);
   });
   gadgets.window.adjustHeight();
 }
